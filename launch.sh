a3b92ec196eeac0d97d40fd16f316ee3a73c61ee
Tiago Campos <13822700+tiagogcampos@users.noreply.github.com>
2025-01-01 11:33:51 +0000

    add sqlite jdbc jar to classpath launch.sh

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ -z $SPARK_HOME ] then
-  SPARK_HOME=/Users/tiagocampos/Software/spark-3.3.2-bin-hadoop3/
+if [ -z "$SPARK_HOME" ]; then
+  export SPARK_HOME="/Users/tiagocampos/Software/spark-3.3.2-bin-hadoop3"
 fi
 
-java -classpath ./target/spark-launcher-test-1.0-SNAPSHOT.jar:$SPARK_HOME/jars/spark-launcher_2.12-3.3.2.jar com.tiagocampos.Launcher ./main.py 2> error.log
+java -classpath "./sqlite-jdbc-3.47.1.0.jar:./target/spark-launcher-test-1.0-SNAPSHOT.jar:$SPARK_HOME/jars/spark-launcher_2.12-3.3.2.jar" com.tiagocampos.Launcher ./main.py 2> error.log
